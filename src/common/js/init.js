2a9453b6b85867ddc4cdee85cb0c069314d405c7
touming123 <junlijiaojiao@163.com>
2017-04-11 13:50:01 +0800

    fix rules bug

--- src/common/js/init.js
+++ src/common/js/init.js
@@ -1,20 +1,19 @@
-import {render} from './render.js';
+import { render } from './render.js';
 
-export function init(){
-	let canvas = document.querySelector("#canvas");
-	let arr = [
-	    [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
-	    [0, 1, 1, 2, 2, 2, 1, 1, 0, 0],
-	    [0, 1, 2, 2, 3, 4, 4, 1, 1, 0],
-	    [1, 1, 3, 2, 1, 4, 3, 4, 1, 0],
-	    [1, 2, 2, 2, 1, 4, 4, 4, 1, 0],
-	    [1, 2, 3, 1, 1, 1, 3, 2, 1, 0],
-	    [1, 2, 2, 3, 2, 3, 2, 1, 1, 0],
-	    [1, 1, 5, 2, 2, 2, 2, 1, 0, 0],
-	    [0, 1, 1, 1, 1, 1, 1, 1, 0, 0]
-	];
-	window.onload = function(){
-		render(canvas,arr);
-	};
+export function init() {
+    let canvas = document.querySelector('#canvas');
+    let arr = [
+        [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+        [0, 1, 1, 2, 2, 2, 1, 1, 0, 0],
+        [0, 1, 2, 2, 3, 4, 4, 1, 1, 0],
+        [1, 1, 3, 2, 1, 4, 3, 4, 1, 0],
+        [1, 2, 2, 2, 1, 4, 4, 4, 1, 0],
+        [1, 2, 3, 1, 1, 1, 3, 2, 1, 0],
+        [1, 2, 2, 3, 2, 3, 2, 1, 1, 0],
+        [1, 1, 5, 2, 2, 2, 2, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 1, 0, 0]
+    ];
+    window.onload = function() {
+        render(canvas, arr);
+    };
 }
-	
