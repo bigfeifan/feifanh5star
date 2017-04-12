d3790bfcef2104a19de1d75e484c1ad9a3dfdd49
kyjo2014 <467346087@qq.com>
2017-04-12 08:24:10 +0800

    增加事件绑定函数

---
+++ src/common/js/bindEvent.js
@@ -0,0 +1,12 @@
+/**
+ * 绑定事件到元素
+ * 
+ * @export
+ * @param {string} Selector css 选择器
+ * @param {string} eventName 事件名字
+ * @param {function} func 回调函数
+ */
+export function bindEvent(Selector, eventName, func) {
+    let element = document.querySelector(Selector)
+    element['on' + eventName] = func
+}
