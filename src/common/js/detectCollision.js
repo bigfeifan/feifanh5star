19044d9b0ad2d38979926b7b33c0d608b370596b
kyjo2014 <467346087@qq.com>
2017-04-13 18:24:53 +0800

    检测碰撞有BUG

--- src/common/js/detectCollision.js
+++ src/common/js/detectCollision.js
@@ -9,6 +9,8 @@
  * @return {any} 返回碰撞状态 // false 未发生碰撞 0墙 1箱子 如果发生碰撞返回状态和碰撞物的位置
  */
 export function detectCollision(status, x, y, dir) {
+    x = +x;
+    y = +y;
     // 移动方向和数字的映射
     const dir2num = {
         'up': 0,
@@ -17,23 +19,35 @@ export function detectCollision(status, x, y, dir) {
         'left': 3
     };
     // 目的地
-    let nextBlock = -1;
+    let nextBlock = {
+        type: -1, // 目的地类型
+        x: null, // 目的横坐标
+        y: null // 目的地纵坐标
+    };
     switch (dir2num[dir]) {
         case 0:
-            nextBlock = status[x - 1][y];
+            nextBlock.type = status[x - 1][y];
+            nextBlock.x = x - 1;
+            nextBlock.y = y;
             break;
         case 1:
-            nextBlock = status[x][y + 1];
+            nextBlock.type = status[x][y + 1];
+            nextBlock.x = x;
+            nextBlock.y = y + 1;
             break;
         case 2:
-            nextBlock = status[x + 1][y];
+            nextBlock.type = status[x + 1][y];
+            nextBlock.x = x + 1;
+            nextBlock.y = y;
             break;
         case 3:
-            nextBlock = status[x][y - 1];
+            nextBlock.type = status[x][y - 1];
+            nextBlock.x = x;
+            nextBlock.y = y - 1;
             break;
         default:
-            nextBlock = -1;
+            nextBlock.type = -1;
             break;
     }
-    return nextBlock === 2 ? false : nextBlock;
+    return nextBlock.type === 2 ? false : nextBlock;
 }
--- src/common/js/update.js
+++ src/common/js/update.js
@@ -10,16 +10,18 @@ import {
  */
 export function update(ctx, dir) {
     const people = document.querySelector('.people'); // 获取人的节点
-    let savedPeople = ctx.curStatus.people // 人的当前状态
+    let savedPeople = ctx.curStatus.people; // 人的当前状态
     let detectCollisionWith = detectCollision.bind(ctx, ctx.curStatus.map); // 为碰撞函数绑定默认参数
-    let firstDetect = detectCollisionWith(savedPeople.x, savedPeople.y, dir); //第一次检测人和周围物体碰撞
-    if (!firstDetect) {
-        move(people, this.option.base, dir);
-    } else if (firstDetect === 3) {
-        let secondDetect = detectCollisionWith(savedPeople.x, savedPeople.y, dir);
-        if (secondDetect) {
+    let firstDetect = detectCollisionWith(savedPeople.x, savedPeople.y, dir); // 第一次检测人和周围物体碰撞
+    console.log(firstDetect);
+    if (!firstDetect ) {
+        move(people, ctx.option.base, dir);
+    } else if (firstDetect.type === 3) {
+        let secondDetect = detectCollisionWith(firstDetect.x, firstDetect.y, dir);
+        let box = ctx.curStatus.map[secondDetect.x][secondDetect.y].object;
+        if (!secondDetect.type) {
+            move(box,ctx.base,dir); // 移动箱子
             move(people, ctx.base, dir); // 移动人
-            move(); // 移动箱子
         } else {
             // do nothing
         }
