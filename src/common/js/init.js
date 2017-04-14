abb621cf9599132aa9f7abae26c8a5cc61e56a6c
kyjo2014 <467346087@qq.com>
2017-04-14 18:15:05 +0800

    bug版本双人无法同时操作

--- src/common/js/init.js
+++ src/common/js/init.js
@@ -5,22 +5,24 @@ import {
     level
 } from './level.js';
 export function init() {
-    let container = document.querySelector('.wrap');
+    let container = document.querySelector('#player1');
+    let container2 = document.querySelector('#player2');
     let arr = level()[0];
     window.onload = function() {
         /* eslint-disable no-new */
-        let player = 1;
+        const player1 = 1;// 用户编号
+        const player2 = 2;
         new Scence(container, arr, {
             wallImage: './static/img/wall.png',
             floorImage: './static/img/floor.png',
             desImage: './static/img/box-out.png',
-            id: player++
+            id: player1
         });
         new Scence(container2, arr, {
             wallImage: './static/img/wall.png',
             floorImage: './static/img/floor.png',
             desImage: './static/img/box-out.png',
-            id: player++
+            id: player2
         });
     };
 }
