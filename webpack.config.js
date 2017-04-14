43f638c6107ec73f7b252e856ff0570564cc7d39
zzx <315084425@qq.com>
2017-04-14 16:01:43 +0800

    注释无用代码

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: './src/app.js',
   output: {
     path: resolve('dist'),
-    publicPath: env === 'production' ? 'https://bigfeifan.github.io/feifanh5star/' : '/dist/',
+    publicPath: env === 'production' ? '/feifanh5star/' : '/dist/',
     filename: 'bundle.js'
   },  
 
