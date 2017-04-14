e2c741715b73215e53cdd22bcf8e338bde0bd520
zzx <315084425@qq.com>
2017-04-14 10:44:59 +0800

    changed consturctor

--- src/common/js/renderStatus.js
+++
@@ -1,9 +0,0 @@
-export function renderIn(dom) {
-    dom.classList.remove('out');
-    dom.classList.add('in');
-}
-
-export function renderOut(dom) {
-    dom.classList.remove('in');
-    dom.classList.add('out');
-}
